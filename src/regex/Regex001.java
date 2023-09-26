5535dc9480252c8c4edecf4014dbd595d04c13cc
adamczyk <lu.adamczyk97@gmail.com>
2023-09-26 18:59:30 +0200

    refactored regex001

--- src/regex/Regex001.java
+++ src/regex/Regex001.java
@@ -4,21 +4,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex001 {
-        private static final Pattern PATTERN = Pattern.compile("(\\+48)?5[0-9]{8}");
 
         public static void main(String[] args) {
-            validate("515332123");
-            validate("+48512346789");
-            validate("515336293");
-            validate("712123");
-            validate("51123");
-        }
-        public static void validate(String text){
-            Matcher matcher = PATTERN.matcher(text);
-            if(matcher.matches()){
-                System.out.println("Prawda dla tekstu: " + text);
-            } else {
-                System.out.println("Fałsz dla tekstu: " + text);
-            }
+            RegexTester tester = new RegexTester("(\\+48)?5[0-9]{8}");
+            tester.validate("515332123");
+            tester.validate("+48512346789");
+            tester.validate("515336293");
+            tester.validate("712123");
+            tester.validate("51123");
         }
 }
