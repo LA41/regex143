b787d62cf77c9a298e8b549c7f6abb83f15991ed
adamczyk <lu.adamczyk97@gmail.com>
2023-09-26 21:00:13 +0200

    regex004 fun with checking date

---
+++ src/regex/Regex004.java
@@ -0,0 +1,7 @@
+package regex;
+
+public class Regex004 {
+    public static void main(String[] args) {
+        RegexTester rt = new RegexTester("[1-9][0-9]{3}[\\-][1-9][0-2]?[\\-][1-3]?[0-9]");
+    }
+}
--- src/regex/RegexReplaceSplit.java
+++ src/regex/RegexReplaceSplit.java
@@ -3,6 +3,8 @@ package regex;
 public class RegexReplaceSplit {
     public static void main(String[] args) {
         String text = "The quick brown fox jumps over the lazy dog";
-
+        String replaced = text.replaceAll("\\w+", "***");
+        System.out.println("original: " + text);
+        System.out.println("replaced: " + replaced);
     }
 }
