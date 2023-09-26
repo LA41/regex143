2ca248a7378561b6d579ac8119883194db84e6c7
adamczyk <lu.adamczyk97@gmail.com>
2023-09-26 18:44:36 +0200

    added +48 to regex

--- src/regex/Regex001.java
+++ src/regex/Regex001.java
@@ -4,22 +4,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Regex001 {
-        private static final Pattern PATTERN = Pattern.compile("5[0-9]{8}");
+        private static final Pattern PATTERN = Pattern.compile("(\\+48)?5[0-9]{8}");
 
         public static void main(String[] args) {
-        /*
-        Prawda dla tekstu: 515332123
-        Prawda dla tekstu: 515336293
-        Fałsz dla tekstu: 415336293Fałsz dla tekstu: 615332123
-        Fałsz dla tekstu: 715332123
-        Fałsz dla tekstu: 712123
-        Fałsz dla tekstu: 51123
-         */
             validate("515332123");
+            validate("+48512346789");
             validate("515336293");
-            validate("415336293");
-            validate("615332123");
-            validate("715332123");
             validate("712123");
             validate("51123");
         }
